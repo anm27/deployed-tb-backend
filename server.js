0be2d83409a027b435bc3d70a5b17679b555ef48
anm222 <anmishra20@gmail.com>
2024-09-03 21:59:29 +0530

    MTB: Backend server.js cors changes

--- server.js
+++ server.js
@@ -15,14 +15,72 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin: [
+      "https://meratravelbuddy.com",
+      "https://mtb.meratravelbuddy.com",
+      "http:localhost:3000",
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    credentials: true,
   },
 });
 
 connectDB();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://meratravelbuddy.com",
+    "http://localhost:3000"
+  );
+  res.header("Access-Control-Allow-Methods", "GET", "POST", "OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.use(
+  cors({
+    origin: [
+      "https://meratravelbuddy.com",
+      "https://mtb.meratravelbuddy.com",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+    ],
+    credentials: true,
+  })
+);
+
+app.options(
+  "*",
+  cors({
+    origin: [
+      "https://meratravelbuddy.com",
+      "https://mtb.meratravelbuddy.com",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use((req, res, next) => {
